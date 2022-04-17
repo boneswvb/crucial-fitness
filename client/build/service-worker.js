5983cf4818ef39a86f870a9838320e8785c5f1de
Wim von Benecke <boneswvb@gmail.com>
2022-04-17 15:51:43 +0200

    17-04-2022

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crucial-fitness/client/build/precache-manifest.f392b501bd7ba5a1eb3343dfbdddfe6d.js"
+  "/crucial-fitness/precache-manifest.6935422e61e0a7ba32f5f1de54de4f40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crucial-fitness/client/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crucial-fitness/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

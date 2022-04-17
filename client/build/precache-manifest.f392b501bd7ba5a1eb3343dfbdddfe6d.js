9f32abdbb7adc6e39063823c32dfc63f2d40bb02
Wim von Benecke <boneswvb@gmail.com>
2022-04-17 15:46:39 +0200

    17-04-2022

---
+++ client/build/precache-manifest.f392b501bd7ba5a1eb3343dfbdddfe6d.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9238ec99d0ecd68160754c32687a8f",
+    "url": "/crucial-fitness/client/build/index.html"
+  },
+  {
+    "revision": "f6aa2ce78d0a17b452b2",
+    "url": "/crucial-fitness/client/build/static/css/2.3809b2e0.chunk.css"
+  },
+  {
+    "revision": "a758ee218027e9830efd",
+    "url": "/crucial-fitness/client/build/static/css/main.675ed896.chunk.css"
+  },
+  {
+    "revision": "f6aa2ce78d0a17b452b2",
+    "url": "/crucial-fitness/client/build/static/js/2.2b62b782.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/crucial-fitness/client/build/static/js/2.2b62b782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a758ee218027e9830efd",
+    "url": "/crucial-fitness/client/build/static/js/main.6faf5ea5.chunk.js"
+  },
+  {
+    "revision": "7466135197754d0a3d46",
+    "url": "/crucial-fitness/client/build/static/js/runtime-main.e53e77ce.js"
+  },
+  {
+    "revision": "ca712c68972ab2782ccf337a4bbf127a",
+    "url": "/crucial-fitness/client/build/static/media/1402472465186.ca712c68.jpg"
+  },
+  {
+    "revision": "33e8bfac26311dbe80ca38b36b8f4680",
+    "url": "/crucial-fitness/client/build/static/media/1402472489425.33e8bfac.jpg"
+  },
+  {
+    "revision": "60b1028c36fbecf703ef732b98cea366",
+    "url": "/crucial-fitness/client/build/static/media/1402472506363.60b1028c.jpg"
+  },
+  {
+    "revision": "14bb54b79e260228e60e6a3b7ba05095",
+    "url": "/crucial-fitness/client/build/static/media/1405422003941.14bb54b7.jpg"
+  },
+  {
+    "revision": "319239e5d2acd3e0e57624be6736b410",
+    "url": "/crucial-fitness/client/build/static/media/19BalancingBenefitsOfThisTwoIngredientMorningTonicWieghtLoss.319239e5.pdf"
+  },
+  {
+    "revision": "99d19b279538492daa1db701cccc75d6",
+    "url": "/crucial-fitness/client/build/static/media/5DayFatLossAcceleratorv3WieghtLoss.99d19b27.pdf"
+  },
+  {
+    "revision": "648476d1b8d7b47b0a0a75ba30780613",
+    "url": "/crucial-fitness/client/build/static/media/BWellReceipyBookEBook.648476d1.pdf"
+  },
+  {
+    "revision": "d9b26632bb6d290136ff7c59bc17fe4b",
+    "url": "/crucial-fitness/client/build/static/media/Coaching.d9b26632.png"
+  },
+  {
+    "revision": "198f60e9f99017c1db2ff85c7ea8ed5e",
+    "url": "/crucial-fitness/client/build/static/media/EatToLiveEBook.198f60e9.jpg"
+  },
+  {
+    "revision": "16e8b4f6c8dc7e0bb5d4aa93c9ebe11b",
+    "url": "/crucial-fitness/client/build/static/media/EvanBrandStressSolutionsEBook.16e8b4f6.jpg"
+  },
+  {
+    "revision": "cc197744c364630945940223613d4525",
+    "url": "/crucial-fitness/client/build/static/media/EvanBrandStressSolutionsEBook.cc197744.pdf"
+  },
+  {
+    "revision": "d4b44086c6244d7283cec8f8b3d76d2e",
+    "url": "/crucial-fitness/client/build/static/media/FB_IMG_1430393108986.d4b44086.jpg"
+  },
+  {
+    "revision": "49fbf08ced899a95d7d32d697e163aeb",
+    "url": "/crucial-fitness/client/build/static/media/FiveDayFattLossAccelerator.49fbf08c.jpg"
+  },
+  {
+    "revision": "99dc88a4947c5aaf7e9633b5d09b68dc",
+    "url": "/crucial-fitness/client/build/static/media/GutBrainHealingRecipesEBook.99dc88a4.jpg"
+  },
+  {
+    "revision": "eb99866d38edc96350f4dce07b9e3216",
+    "url": "/crucial-fitness/client/build/static/media/GutBrainHealingRecipesEBook.eb99866d.pdf"
+  },
+  {
+    "revision": "81aa3ad5d53c3c80124a863de23e2b14",
+    "url": "/crucial-fitness/client/build/static/media/HealthInfo.81aa3ad5.jpg"
+  },
+  {
+    "revision": "4e17ac99b1d14726e9c11ccb559ddecb",
+    "url": "/crucial-fitness/client/build/static/media/IMG_25952646232458.4e17ac99.jpeg"
+  },
+  {
+    "revision": "c1eb3e517154adefca91d396e5023002",
+    "url": "/crucial-fitness/client/build/static/media/IMG_25969002780466.c1eb3e51.jpeg"
+  },
+  {
+    "revision": "13dd237bbadb6e7d5f7af026b4f78309",
+    "url": "/crucial-fitness/client/build/static/media/IMG_51625079800314.13dd237b.jpeg"
+  },
+  {
+    "revision": "b5fa63d0c41e9ceddcb76d9c57445cd7",
+    "url": "/crucial-fitness/client/build/static/media/IMG_51666736418292.b5fa63d0.jpeg"
+  },
+  {
+    "revision": "0d7cffb396a65e851391fa69a3d6969f",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadDiabesityRP.0d7cffb3.pdf"
+  },
+  {
+    "revision": "52c26759473b66dc530189bddf5f4ce7",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadDiabesityRPWeb.52c26759.jpg"
+  },
+  {
+    "revision": "091e03954260ab15ab89b4d943ae9326",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadDiabetesBook.091e0395.jpg"
+  },
+  {
+    "revision": "1e77929732f95f7d028ec3d29a70c5e6",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadDiabetesBook.1e779297.pdf"
+  },
+  {
+    "revision": "c9eed0d648ab8fde194030e2ab18531b",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadEatWell.c9eed0d6.pdf"
+  },
+  {
+    "revision": "00d221b4d16ec3b1e3841cfd3d9d3fc2",
+    "url": "/crucial-fitness/client/build/static/media/InterestingReadTricksAndTipsForDrinkingMoreWater..00d221b4.pdf"
+  },
+  {
+    "revision": "e43aa4d05ddab80efecbf44628112e44",
+    "url": "/crucial-fitness/client/build/static/media/MealPrepping7DayHealthyFood.e43aa4d0.jpg"
+  },
+  {
+    "revision": "b29100d603f095ecdb5a97f03c63b347",
+    "url": "/crucial-fitness/client/build/static/media/MealPrepping7DayHealthyFoodMealPlan.b29100d6.pdf"
+  },
+  {
+    "revision": "07f644564db39991273500cbdf88fe8d",
+    "url": "/crucial-fitness/client/build/static/media/MenopauseEBook.07f64456.pdf"
+  },
+  {
+    "revision": "e6310ca22c02f749c6273b0fa924d418",
+    "url": "/crucial-fitness/client/build/static/media/MenopauseEBook.e6310ca2.jpg"
+  },
+  {
+    "revision": "aca2896ea14a573c8aa7cc9f8c8a6225",
+    "url": "/crucial-fitness/client/build/static/media/NineteenBalancingBenefitsOf.aca2896e.jpg"
+  },
+  {
+    "revision": "8f90a2b15f25cc1ade296283ef8fdb78",
+    "url": "/crucial-fitness/client/build/static/media/Photography.8f90a2b1.jpg"
+  },
+  {
+    "revision": "432f51faf2d62fbbc5c39f15d4813077",
+    "url": "/crucial-fitness/client/build/static/media/Reading.432f51fa.jpg"
+  },
+  {
+    "revision": "d36f6ee106df5de5d585d7c6afd9759f",
+    "url": "/crucial-fitness/client/build/static/media/TheRedTeaDetox.d36f6ee1.jpg"
+  },
+  {
+    "revision": "92d46a5b678ddb57d7ef097fafbfc855",
+    "url": "/crucial-fitness/client/build/static/media/TheRedTeaDetoxFreeWieghtLoss.92d46a5b.pdf"
+  },
+  {
+    "revision": "1509050c0ff7f53e341452b1fe6a1ca7",
+    "url": "/crucial-fitness/client/build/static/media/TheUltimateGuideToDetoxing.1509050c.jpg"
+  },
+  {
+    "revision": "2a684138757e30aef47a47cafd2a24f8",
+    "url": "/crucial-fitness/client/build/static/media/TheUltimateGuideToDetoxingWieghtLoss.2a684138.pdf"
+  },
+  {
+    "revision": "458388343c56219a731fa0652bc69290",
+    "url": "/crucial-fitness/client/build/static/media/TopupplementsForEnergyEnhan.45838834.jpg"
+  },
+  {
+    "revision": "130711ebb997e215c0abcb03af1e463b",
+    "url": "/crucial-fitness/client/build/static/media/TopupplementsForEnergyEnhancementEBook.130711eb.pdf"
+  },
+  {
+    "revision": "ffbf463b2e5f2e7e9b16d316a70bd3f3",
+    "url": "/crucial-fitness/client/build/static/media/UltimateGuideToDetoxingEBook.ffbf463b.pdf"
+  },
+  {
+    "revision": "f6d7dcaf001d45ee1d2d688f9452770e",
+    "url": "/crucial-fitness/client/build/static/media/businessLogo.f6d7dcaf.png"
+  },
+  {
+    "revision": "312db5a43cfbc04170302e1980225d09",
+    "url": "/crucial-fitness/client/build/static/media/receipies.312db5a4.jpg"
+  },
+  {
+    "revision": "0c8036af444ca5ed7b03f689a7a01149",
+    "url": "/crucial-fitness/client/build/static/media/rehabilitation.0c8036af.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b139ff712954e4645a881c320eb5ad60.js"
+  "/crucial-fitness/client/build/precache-manifest.f392b501bd7ba5a1eb3343dfbdddfe6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crucial-fitness/client/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
